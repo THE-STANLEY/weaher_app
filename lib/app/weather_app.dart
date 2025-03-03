57a2c0ab4fd72b408c42423732cb41ecb0abd407
THE-STANLEY <lilstanley@inbox.ru>
2025-03-03 17:55:08 +0500

    init repository

---
+++ lib/app/weather_app.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class WeatherApp extends StatelessWidget {
+  const WeatherApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Weather App',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+      ),
+    );
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+import 'package:weather/app/weather_app.dart';
+
+void main() {
+  runApp(const WeatherApp());
+}
